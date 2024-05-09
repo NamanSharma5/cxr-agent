8d846dc5c88e6b15d0b5a74239888f064c450059
Naman Sharma <naman.sharma20@imperial.ac.uk>
2024-05-09 17:21:52 +0100

    added outline for phrase grounder, generation engine taking pathology detector activations, gpu utils

---
+++ base_agent/phrase_grounder.py
@@ -0,0 +1,43 @@
+import os
+os.environ['HF_HOME'] = '/vol/biomedic3/bglocker/ugproj2324/nns20/cxr-agent/.hf_cache' ## THIS HAS TO BE BEFORE YOU IMPORT TRANSFORMERS
+
+from health_multimodal.common.visualization import plot_phrase_grounding_similarity_map
+from health_multimodal.text import get_bert_inference
+from health_multimodal.text.utils import BertEncoderType
+from health_multimodal.image import get_image_inference
+from health_multimodal.image.utils import ImageModelType
+from health_multimodal.vlp import ImageTextInferenceEngine
+
+from agent_utils import select_best_gpu
+from abc import ABC, abstractmethod
+
+
+class PhraseGrounder(ABC):
+
+    @abstractmethod
+    def get_similiarity_map(self, phrase: str, image_path: str):
+        pass
+
+    @abstractmethod 
+    def get_pathology_lateral_position(self, patholgy: str, image_path: str) -> str:
+        pass
+
+
+class BioVilTPhraseGrounder(PhraseGrounder):
+
+    def __init__(self):
+        self.text_inference = get_bert_inference(BertEncoderType.BIOVIL_T_BERT)
+        self.image_inference = get_image_inference(ImageModelType.BIOVIL_T)  
+        self.image_text_inference_engine = ImageTextInferenceEngine(image_inference_engine=self.image_inference, text_inference_engine=self.text_inference)
+        self.device = select_best_gpu()
+        self.image_text_inference_engine.to(self.device)
+
+    def get_similiarity_map(self, image_path: str, phrase: str, ):
+        return self.image_text_inference_engine.get_similarity_map_from_raw_data(
+            image_path=image_path,
+            query_text=phrase,
+            interpolation="bilinear"
+        )
+
+    def get_pathology_lateral_position(self, pathology: str, image_path: str) -> str:
+        return "TODO"
